f8a61041023e22f54eb4e8d577c89ed62dd91153
Jayson Virissimo <jayson.virissimo@asu.edu>
2015-08-16 17:25:58 -0700

    finish script and generate new resumes

--- documents/generator.rb
+++ documents/generator.rb
@@ -4,3 +4,9 @@
 `resume export pdf.pdf --format pdf --theme short`
 # work around until markdown is properly supported
 `resume export markdown.html --format markdown --theme markdown`
+`cp html.html resume.html`
+`cp pdf.pdf resume.pdf`
+`cp markdown.html resume.md`
+`rm html.html`
+`rm pdf.pdf`
+`rm markdown.html`
