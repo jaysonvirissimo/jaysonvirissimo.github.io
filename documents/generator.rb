bf2052ef8f4b1e4578a4444b6c32b92749d6cb44
Jayson Virissimo <jayson.virissimo@asu.edu>
2015-08-16 17:19:48 -0700

    add script to generate new resumes in all formats

---
+++ documents/generator.rb
@@ -0,0 +1,6 @@
+`sudo npm install -g resume-cli`
+`resume test`
+`resume export html.html --format html --theme elegant`
+`resume export pdf.pdf --format pdf --theme short`
+# work around until markdown is properly supported
+`resume export markdown.html --format markdown --theme markdown`
