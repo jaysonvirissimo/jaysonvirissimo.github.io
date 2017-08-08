5b4dc39fb9e3a2fe20b3ebf61dd1aa4e496b4148
Jayson Virissimo <jayson.virissimo@asu.edu>
2017-08-07 18:10:49 -0700

    Failing specs block deploys

--- deploy.rb
+++ deploy.rb
@@ -1,1 +1,10 @@
-`git subtree push --prefix src origin master`
+test_output = `npm test`
+
+if test_output.include?('failing')
+  puts 'Some tests have failed, skipping deployment.'
+elsif test_output.include?('passing')
+  puts 'All tests passing, deploying now...'
+  `git subtree push --prefix src origin master`
+else
+  puts 'Something went wrong with the test run.'
+end
