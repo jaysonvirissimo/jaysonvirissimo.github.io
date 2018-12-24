30f3b2da4053896a3a5287fc73b47d6257fadeb5
Jayson Virissimo <jayson.virissimo@asu.edu>
2018-12-24 15:00:32 -0700

    Restyle portfolio section

--- test/homepage.spec.js
+++ test/homepage.spec.js
@@ -32,70 +32,6 @@ describe('My professional website', () => {
     });
   });
 
-  // TODO: Add biography
-  describe('marketing grid', () => {
-    let marketing;
-
-    beforeEach(() => marketing = mockDocument.querySelector('.marketing'));
-
-    it('should exist', () => {
-      assert(marketing, 'There should be a `.marketing` element.');
-    });
-
-    it('should have a cointainer', () => {
-      const el = marketing.querySelector('.nes-container');
-      assert(el);
-    });
-
-    it('should have a row', () => {
-      const el = marketing.querySelector('.nes-container .row');
-      assert(el);
-    });
-
-    describe('columns', () => {
-      let columns = [];
-
-      beforeEach(() => {
-        if (marketing) {
-          columns = Array.from(marketing.querySelectorAll('.row .col-sm-4, .row .col-md-4, .row .col-lg-4'));
-        }
-      });
-
-      it('should exist', () => {
-        assert(columns.length === 3, 'The `.row` should have three column elements.');
-      });
-
-      it('should have a Font Awesome icon', () => {
-        columns.forEach(column => {
-          const icon = column.querySelector('.fa');
-          const iconClasses = Array.from(icon.classList);
-          assert(icon, 'All row columns should have an icon.');
-          assert(
-            iconClasses.find(className => className.startsWith('fa-')),
-            'Icon classes need to start with `fa-` or they will not work.'
-          );
-        });
-      });
-
-      it('should have a title', () => {
-        columns.forEach(column => {
-          const h2 = column.querySelector('h2');
-          assert(h2, 'All columns should have an `h2` element.');
-          assert(h2.textContent.trim() !== '', 'The `h2` elements should not be empty.');
-        })
-      });
-
-      it('should have a paragraph', () => {
-        columns.forEach(column => {
-          const p = column.querySelector('p');
-          assert(p, 'All columns should have a `p` element.');
-          assert(p.textContent.trim() !== '', 'The `p` elements should not be empty.');
-        });
-      });
-    });
-  });
-
-  // TODO: Update resume
   describe('resume section', () => {
     let resume;
 
@@ -113,6 +49,52 @@ describe('My professional website', () => {
     it('should contain links to resumes', () => {
       const links = Array.from(resume.querySelectorAll('a'));
       assert(links.length === 3);
+    });
+  });
+
+  // TODO: Add biography
+  describe('portfolio section', () => {
+    let portfolio;
+
+    beforeEach(() => portfolio = mockDocument.querySelector('#portfolio'));
+
+    it('should exist', () => {
+      assert(portfolio);
+    });
+
+    it('should have a title', () => {
+      const h2 = portfolio.querySelector('h2');
+      assert(h2.textContent.trim().toLowerCase() === 'portfolio');
+    });
+
+    describe('projects', () => {
+      let projects = [];
+
+      beforeEach(() => {
+        if (portfolio) {
+          projects = Array.from(portfolio.querySelectorAll('.project'));
+        }
+      });
+
+      it('should exist', () => {
+        assert(projects.length);
+      });
+
+      it('should have titles', () => {
+        projects.forEach(project => {
+          const h3 = project.querySelector('h3');
+          assert(h3);
+          assert(h3.textContent.trim() !== '');
+        })
+      });
+
+      it('should have a description', () => {
+        projects.forEach(project => {
+          const p = project.querySelector('p.description');
+          assert(p);
+          assert(p.textContent.trim() !== '');
+        });
+      });
     });
   });
 
